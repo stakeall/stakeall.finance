f44dedea30113102cd1bfcd20dfa38886e1a45b4
cryptostaker2021 <cryptostaker2021@gmail.com>
2021-06-23 22:20:55 +0530

    Added script to test borrow and swap

---
+++ scripts/integration_borrow_swap_and_stake.js
@@ -0,0 +1,108 @@
+
+
+const Web3 = require('web3');
+const axios = require('axios');
+const abiDecoder = require('abi-decoder');
+const UserWallet = require('../artifacts/contracts/BitStakerRegistry.sol/UserWallet.json');
+const ERC20 = require('../artifacts/contracts/IERC20Interface.sol/IERC20Interface.json');
+const OneInch = require('../artifacts/contracts/protocols/1inch/1Inch.sol/OneInch.json');
+const ContractAddresses = require('../localhost_contract.json');
+const AaveProtocolArtifacts = require('../artifacts/contracts/protocols/aave/ConnectV2AaveV2.sol/AaveResolver.json');
+
+const perform = async () => {
+
+  const privateKey = "ac0974bec39a17e36ba4a6b4d238ff944bacb478cbed5efcae784d7bf4f2ff80";
+  const web3 = new Web3('http://localhost:8545');
+  const account = web3.eth.accounts.privateKeyToAccount(privateKey)
+  web3.eth.accounts.wallet.add(account);
+
+  const aaveProtocolAddress = ContractAddresses.AAVEProtocol;
+
+  const aaveInstance = new web3.eth.Contract(AaveProtocolArtifacts.abi, aaveProtocolAddress);
+
+  const borrowTokenAddress = '0xdac17f958d2ee523a2206206994597c13d831ec7';
+  const sourceToken = '0xEeeeeEeeeEeEeeEeEeEeeEEEeeeeEeeeeeeeEEeE';
+  const amount = '1000000000000000000';
+  const borrowAmount = '1000000000';
+  const rateMode = '1';
+  const slippage = 1;
+  const destinationToken = '0xc944e90c64b2c07662a292be6244bdf05cda44a7';
+  const fromAddress = account.address;
+
+  const borrowERC20Instance = new web3.eth.Contract(ERC20.abi, borrowTokenAddress);
+  const destinationTokenERC20Instance = new web3.eth.Contract(ERC20.abi, destinationToken);
+
+  const userWalletInstance = new web3.eth.Contract(UserWallet.abi, ContractAddresses.userWalletAddress);
+
+  const aaveDepositAndBorrowEncodedData = aaveInstance.methods.depositAndBorrow(
+    sourceToken,
+    borrowTokenAddress,
+    amount,
+    borrowAmount,
+    rateMode
+  ).encodeABI();
+
+  console.log('aaveDepositAndBorrowEncodedData :', aaveDepositAndBorrowEncodedData);
+
+
+// One Inch 
+
+const request = 
+`https://api.1inch.exchange/v3.0/1/swap?fromTokenAddress=${borrowTokenAddress}&toTokenAddress=${destinationToken}&amount=${borrowAmount}&slippage=${slippage}&fromAddress=${ContractAddresses.userWalletAddress}&disableEstimate=true`;
+    console.log(request);
+    const swapResponse = await axios.get(request);
+
+    console.log('destination Amount from swap  ' +swapResponse.data.toTokenAmount);
+
+    console.log('Contract address '+swapResponse.data.tx.to);
+
+    const oneInchProxy = new web3.eth.Contract(OneInch.abi, ContractAddresses.oneInchProtocol);
+
+    const swapTransactionEncodedData = oneInchProxy.methods.swap(
+        borrowAmount,
+        borrowTokenAddress,
+        destinationToken,
+        swapResponse.data.tx.to,
+        swapResponse.data.tx.data,
+        0
+    ).encodeABI();
+
+  const transaction = userWalletInstance.methods.executeMulti(
+    [aaveProtocolAddress, ContractAddresses.oneInchProtocol],
+    [aaveDepositAndBorrowEncodedData, swapTransactionEncodedData],
+//     [ ContractAddresses.oneInchProtocol],
+//    [ swapTransactionEncodedData],
+    1,
+    1
+  );
+  const gas = await transaction.estimateGas({
+    from: fromAddress,
+    value: amount
+  });
+
+  console.log('gas : ', gas);
+
+  const executeReceipt = await transaction.send({
+    from: fromAddress,
+    gas,
+    value: amount
+  }).on('transactionHash', (hash) => {
+    console.log(" approval hash " + hash);
+  });
+
+  
+  abiDecoder.addABI(OneInch.abi);
+  abiDecoder.addABI(AaveProtocolArtifacts.abi);
+  abiDecoder.addABI(UserWallet.abi);
+
+  const receipt = await web3.eth.getTransactionReceipt(executeReceipt.transactionHash);
+  const decodedLogs = abiDecoder.decodeLogs(receipt.logs);
+
+  console.log(JSON.stringify(decodedLogs));
+
+  console.log('borrow value of user wallet ', await borrowERC20Instance.methods.balanceOf(ContractAddresses.userWalletAddress).call());
+  console.log('borrow value of user wallet ', await destinationTokenERC20Instance.methods.balanceOf(ContractAddresses.userWalletAddress).call());
+
+}
+
+perform().then(() => console.log('done'));
