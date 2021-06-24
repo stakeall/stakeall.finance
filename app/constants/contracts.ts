a5e0159292992f5e19a90fb7ffcd639c3c90a9d6
robin <robineth2016@gmail.com>
2021-06-25 00:05:26 +0530

    Added one inch abi and addresses in constants

--- app/constants/contracts.ts
+++ app/constants/contracts.ts
@@ -3,4 +3,8 @@ export const graphProtocol = '0xd8e275e1b5A31Fed1Ac75370E4F834f1ce85fCA1';
 
 export const graphToken = '0x54Fe55d5d255b8460fB3Bc52D5D676F9AE5697CD';
 
+export const oneInch = '0x99bbA657f2BbC93c02D617f8bA121cB8Fc104Acf';
+
 export const ZERO_ADDRESS = '0x0000000000000000000000000000000000000000';
+
+export const ETH_TOKEN = '0xEeeeeEeeeEeEeeEeEeEeeEEEeeeeEeeeeeeeEEeE';
