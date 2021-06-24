5c855ec5041f01428c6e83570fbcf54f903dd287
Antony Syriac <antonysyriac@Antonys-MacBook-Pro.local>
2021-06-25 00:36:47 +0530

    refactor useBitstake

--- app/api/api.ts
+++ app/api/api.ts
@@ -14,5 +14,18 @@ const api = axios.create({
 export const covalent = {
     getAllBalance: (chainId: string | number, address: string) => {
         return api.get<{}, BalanceResponse>(`${covalentBaseUrl}/v1/${chainId}/address/${address}/balances_v2/?&key=${covalentKey}`)
+    },
+
+}
+
+export const oneInchApi = {
+    getSwapDetails: (sourceToken: string, destinationToken: string, sourceTokenAmount: string, slippage: string, onChainWalletAddress: string) => {
+        const request = `https://api.1inch.exchange/v3.0/1/swap?fromTokenAddress=${sourceToken}&toTokenAddress=${destinationToken}&amount=${sourceTokenAmount}&slippage=${slippage}&fromAddress=${onChainWalletAddress}&disableEstimate=true`;
+        return api.get(request);
+    },
+
+    getEstimatedSwapDetails: (sourceToken: string, destinationToken: string, swapAmount: string, slippage: string, onChainWalletAddress: string) => {
+        const request = `https://api.1inch.exchange/v3.0/1/swap?fromTokenAddress=${sourceToken}&toTokenAddress=${destinationToken}&amount=${swapAmount}&slippage=${slippage}&fromAddress=${onChainWalletAddress}&disableEstimate=true`;
+        return api.get(request);
     }
 }
